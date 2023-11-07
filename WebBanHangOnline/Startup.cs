7403962bb3595cbcce8cfe07296d51fb88bd7572
Tran Dinh Tai <trandinhtai169@gmail.com>
2023-11-07 21:39:29 +0700

    startup

--- WebBanHangOnline/Startup.cs
+++ WebBanHangOnline/Startup.cs
@@ -8,7 +8,7 @@ namespace WebBanHangOnline
     {
         public void Configuration(IAppBuilder app)
         {
-            //ConfigureAuth(app);
+            ConfigureAuth(app);
             app.MapSignalR();
         }
     }
